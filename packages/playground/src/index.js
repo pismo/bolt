b48cb45fb51b4efbe2551d1db1460f6f35e4fbbf
Gustavo Giserman <gugiserman@gmail.com>
2018-01-11 19:06:36 -0200

    Lint typography scss imports

--- packages/playground/src/index.js
+++ packages/playground/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import typography from '@pismo/bolt-typography'
 import App from './App'
 
 render(
   <App />,
   document.getElementById('app-container'),
 )
-
-typography.injectStyles()
 
 export default App
