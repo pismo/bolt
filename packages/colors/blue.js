ac69b5b5fc61988adface35f59dbf003be6cbd1a
Gustavo Giserman <gugiserman@gmail.com>
2018-01-12 12:35:33 -0200

    Implement sass variable parser and integrate it to precommit hook

---
+++ packages/colors/blue.js
@@ -0,0 +1,8 @@
+{
+  "blue": "#3bb2dd",
+  "blue500": "#3bb2dd",
+  "blue700": "#0b88b6",
+  "blue400": "#3bb2dd",
+  "blue200": "#77cfee",
+  "blue100": "#c3e9f6"
+}
---
+++ packages/colors/gray.js
@@ -0,0 +1,13 @@
+{
+  "gray": "#8692a6",
+  "gray500": "#8692a6",
+  "gray900": "#151a21",
+  "gray800": "#2c3644",
+  "gray700": "#4f5a6a",
+  "gray600": "#6e7a8c",
+  "gray400": "#a2aec2",
+  "gray300": "#c1c7d4",
+  "gray200": "#d6dae2",
+  "gray100": "#eceef2",
+  "gray50": "#fafafa"
+}
