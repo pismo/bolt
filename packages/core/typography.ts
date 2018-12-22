6e468af3cbbfcf12327365d21eb204f97c8333d1
Guilherme Decampo <guilherme.decampo@gmail.com>
2018-12-22 15:57:37 -0200

    feat(typography): add styles and updates

--- packages/core/typography.ts
+++ packages/core/typography.ts
@@ -4,7 +4,7 @@ const stylesToWeight = (style: string): string => {
   const stylestoWeightMap = {
     light: 300,
     regular: 400,
-    bold: 600,
+    bold: 700,
     black: 900,
     default: 400,
   }
@@ -13,41 +13,47 @@ const stylesToWeight = (style: string): string => {
 }
 
 export interface BasicProps {
-  fontStyle: string
+  fontStyle?: string
 }
 
 export const H1 = styled.h1<BasicProps>`
+  font-size: 2.075rem;
   font-weight: ${({ fontStyle = '' }) => stylesToWeight(fontStyle)};
   font-style: ${({ fontStyle = '' }) => fontStyle.split(' ')[1] || 'normal'};
 `
 
 export const H2 = styled.h2<BasicProps>`
-  font-weight: ${({ fontStyle }) => stylesToWeight(fontStyle)};
+  font-size: 1.725rem;
+  font-weight: ${({ fontStyle = '' }) => stylesToWeight(fontStyle)};
   font-style: ${({ fontStyle = '' }) => fontStyle.split(' ')[1] || 'normal'};
 `
 
 export const H3 = styled.h3<BasicProps>`
-  font-weight: ${({ fontStyle }) => stylesToWeight(fontStyle)};
+  font-size: 1.4375rem;
+  font-weight: ${({ fontStyle = '' }) => stylesToWeight(fontStyle)};
   font-style: ${({ fontStyle = '' }) => fontStyle.split(' ')[1] || 'normal'};
 `
 
 export const H4 = styled.h4<BasicProps>`
-  font-weight: ${({ fontStyle }) => stylesToWeight(fontStyle)};
+  font-size: 1.2rem;
+  font-weight: ${({ fontStyle = '' }) => stylesToWeight(fontStyle)};
   font-style: ${({ fontStyle = '' }) => fontStyle.split(' ')[1] || 'normal'};
 `
 
 export const P = styled.p<BasicProps>`
-  font-weight: ${({ fontStyle }) => stylesToWeight(fontStyle)};
+  font-weight: ${({ fontStyle = '' }) => stylesToWeight(fontStyle)};
   font-style: ${({ fontStyle = '' }) => fontStyle.split(' ')[1] || 'normal'};
 `
 
 export const Small = styled.small<BasicProps>`
+  font-size: 0.83125rem;
   color: ${({ theme }) => theme.fontColorSmall};
-  font-weight: ${({ fontStyle }) => stylesToWeight(fontStyle)};
+  font-weight: ${({ fontStyle = '' }) => stylesToWeight(fontStyle)};
   font-style: ${({ fontStyle = '' }) => fontStyle.split(' ')[1] || 'normal'};
 `
 
 export const Tiny = styled.small<BasicProps>`
-  font-weight: ${({ fontStyle }) => stylesToWeight(fontStyle)};
+  font-size: 0.69375rem;
+  font-weight: ${({ fontStyle = '' }) => stylesToWeight(fontStyle)};
   font-style: ${({ fontStyle = '' }) => fontStyle.split(' ')[1] || 'normal'};
 `
