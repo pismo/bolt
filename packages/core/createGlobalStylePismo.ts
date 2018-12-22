d9bc909c9a58abbe1f514b471c08eda3b7498420
Guilherme Decampo <guilherme.decampo@gmail.com>
2018-12-22 15:59:46 -0200

    feat(globalStyle): export baseStyle

--- packages/core/createGlobalStylePismo.ts
+++ packages/core/createGlobalStylePismo.ts
@@ -6,14 +6,19 @@ import { ThemeInterface } from './themePismo'
 
 export type createGlobalStylePismoI = (customStyle: string) => ThemedGlobalStyledClassProps<{}, ThemeInterface>
 
+export const baseStyle = (theme: ThemeInterface) => `
+  @import url('https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i');
+  font-family: ${theme.fontFamily};
+  color: ${theme.fontColor};
+  font-size: ${theme.fontSizeBase};
+  line-height: ${theme.lineHeightBase};
+  background-color: ${theme.background};
+`
+
 export const createGlobalStylePismo: createGlobalStylePismoI = (customStyle = '') => createGlobalStyle`
   ${normalize()}
   html, body {
-    font-family: ${({ theme }) => theme.fontFamily};
-    color: ${({ theme }) => theme.fontColor};
-    font-size: ${({ theme }) => theme.fontSizeBase};
-    line-height: ${({ theme }) => theme.lineHeightBase};
-    background-color: ${({ theme }) => theme.background};
+    ${({ theme }) => baseStyle(theme)}
   }
   ${customStyle}
 `
