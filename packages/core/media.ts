52da082e4db111100c211032fae1be8d2f6a0073
Guilherme Decampo <guilherme.decampo@gmail.com>
2018-12-28 15:51:06 -0200

    feat(styled): export withTheme and add media helper

---
+++ packages/core/media.ts
@@ -0,0 +1,19 @@
+import * as styledComponents from 'styled-components'
+const { css } = styledComponents
+
+const BASE_FONT_SIZE = 16
+const SIZES = {
+  desktop: 1025,
+  tablet: 900,
+  mobile: 737,
+}
+
+export const media = Object.keys(SIZES).reduce((acc, label) => {
+  const mediaStart = label.includes('desktop') ? 'min' : 'max'
+  acc[label] = (args: TemplateStringsArray) => css`
+    @media (${mediaStart}-width: ${SIZES[label] / BASE_FONT_SIZE}em) {
+      ${css(args)}
+    }
+  `
+  return acc
+}, {})
