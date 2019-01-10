40459f5a7ab6ea478d7887ec5f78665170d86915
Jeova <jeovapereira2000@live.com>
2019-01-10 14:34:53 -0300

    test(core): update test for export of Box

--- packages/core/index.test.tsx
+++ packages/core/index.test.tsx
@@ -4,7 +4,7 @@ import {
   createGlobalStylePismo,
   css,
   Flex,
-  Grid,
+  Box,
   H1,
   H2,
   H3,
@@ -18,100 +18,100 @@ import {
   ThemeProvider,
   Tiny,
   withTheme,
-} from "./index"
+} from './index'
 
-describe("index export test", () => {
-  it("should export the styled correctly", () => {
+describe('index export test', () => {
+  it('should export the styled correctly', () => {
     expect(styled).not.toBeNull()
     expect(styled).toBeDefined()
   })
 
-  it("should export the createGlobalStylePismo correctly", () => {
+  it('should export the createGlobalStylePismo correctly', () => {
     expect(createGlobalStylePismo).not.toBeNull()
     expect(createGlobalStylePismo).toBeDefined()
   })
 
-  it("should export the createGlobalStyle correctly", () => {
+  it('should export the createGlobalStyle correctly', () => {
     expect(createGlobalStyle).not.toBeNull()
     expect(createGlobalStyle).toBeDefined()
   })
 
-  it("should export the themePismo correctly", () => {
+  it('should export the themePismo correctly', () => {
     expect(themePismo).not.toBeNull()
     expect(themePismo).toBeDefined()
   })
 
-  it("should export the css correctly", () => {
+  it('should export the css correctly', () => {
     expect(css).not.toBeNull()
     expect(css).toBeDefined()
   })
 
-  it("should export the keyframes correctly", () => {
+  it('should export the keyframes correctly', () => {
     expect(keyframes).not.toBeNull()
     expect(keyframes).toBeDefined()
   })
 
-  it("should export the ThemeProvider correctly", () => {
+  it('should export the ThemeProvider correctly', () => {
     expect(ThemeProvider).not.toBeNull()
     expect(ThemeProvider).toBeDefined()
   })
 
-  it("should export the withTheme correctly", () => {
+  it('should export the withTheme correctly', () => {
     expect(withTheme).not.toBeNull()
     expect(withTheme).toBeDefined()
   })
 
-  it("should export the Grid correctly", () => {
-    expect(Grid).not.toBeNull()
-    expect(Grid).toBeDefined()
+  it('should export the Box correctly', () => {
+    expect(Box).not.toBeNull()
+    expect(Box).toBeDefined()
   })
 
-  it("should export the Flex correctly", () => {
+  it('should export the Flex correctly', () => {
     expect(Flex).not.toBeNull()
     expect(Flex).toBeDefined()
   })
 
-  it("should export the H1 correctly", () => {
+  it('should export the H1 correctly', () => {
     expect(H1).not.toBeNull()
     expect(H1).toBeDefined()
   })
 
-  it("should export the H2 correctly", () => {
+  it('should export the H2 correctly', () => {
     expect(H2).not.toBeNull()
     expect(H2).toBeDefined()
   })
 
-  it("should export the H3 correctly", () => {
+  it('should export the H3 correctly', () => {
     expect(H3).not.toBeNull()
     expect(H3).toBeDefined()
   })
 
-  it("should export the H4 correctly", () => {
+  it('should export the H4 correctly', () => {
     expect(H4).not.toBeNull()
     expect(H4).toBeDefined()
   })
 
-  it("should export the P correctly", () => {
+  it('should export the P correctly', () => {
     expect(P).not.toBeNull()
     expect(P).toBeDefined()
   })
 
-  it("should export the Small correctly", () => {
+  it('should export the Small correctly', () => {
     expect(Small).not.toBeNull()
     expect(Small).toBeDefined()
   })
 
-  it("should export the Tiny correctly", () => {
+  it('should export the Tiny correctly', () => {
     expect(Tiny).not.toBeNull()
     expect(Tiny).toBeDefined()
   })
 
-  it("should export the colors correctly", () => {
+  it('should export the colors correctly', () => {
     expect(colors).not.toBeNull()
     expect(colors).toBeDefined()
   })
 
-  it("should export the media correctly", () => {
+  it('should export the media correctly', () => {
     expect(media).not.toBeNull()
     expect(media).toBeDefined()
   })
