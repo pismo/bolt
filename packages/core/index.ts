93190763dc712424a2131923ae35aa1f3849a05b
Jeova <jeovapereira2000@live.com>
2019-01-10 14:28:04 -0300

    fix(core): wrong export on index

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -4,7 +4,7 @@ export { styled }
 export { createGlobalStylePismo } from './createGlobalStylePismo'
 export { themePismo } from './themePismo'
 export { css, createGlobalStyle, keyframes, ThemeProvider, withTheme } from './styled'
-export { Grid, Flex } from '@rebass/grid'
+export { Box, Flex } from '@rebass/grid'
 export { H1, H2, H3, H4, P, Small, Tiny } from './typography'
 export { colors } from './colors'
 export { media } from './media'
