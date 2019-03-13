53b9fe41a1948e602218586373384e6a2bd4ea7f
Jefferson <jeffersonhcarvalho@gmail.com>
2019-03-13 11:22:17 -0300

    feat(Storybook): install and configure addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,16 @@
-import { configure } from '@storybook/react'
+import { configure, addDecorator, addParameters } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withInfo } from '@storybook/addon-info'
+
+addParameters({
+  options: {
+    name: 'Bolt',
+    addonPanelInRight: true,
+  },
+})
+
+addDecorator(withKnobs)
+addDecorator(withInfo)
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../packages', true, /.stories.tsx$/)
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,6 +6,9 @@ module.exports = ({ config, mode }) => {
       {
         loader: require.resolve('awesome-typescript-loader'),
       },
+      {
+        loader: require.resolve('react-docgen-typescript-loader'),
+      },
     ],
   })
   config.resolve.extensions.push('.ts', '.tsx')
