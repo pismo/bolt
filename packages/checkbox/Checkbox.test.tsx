dad1d7f6f1026521c6cf564e66577e6957c38411
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-04-05 11:20:22 -0300

    feat(checkbox-test): add tests for checked and unchecked

--- packages/checkbox/Checkbox.test.tsx
+++ packages/checkbox/Checkbox.test.tsx
@@ -4,15 +4,26 @@ import { render } from 'react-testing-library'
 import { Checkbox } from './Checkbox'
 
 describe('Checkbox test', () => {
-  it('Render a checkbox default', () => {
-    const { container, getByText } = render(
-      <Checkbox name={'testing-checkbox'} onChange={() => null} checked={false}>
-        Testing
-      </Checkbox>,
-    )
+  it('Render a checkbox unchecked', () => {
+    const { container, getByText } = render(<Checkbox name={'testing-checkbox'}>Testing</Checkbox>)
 
     expect(container).toBeInTheDocument()
     expect(container).toBeVisible()
     expect(getByText('Testing')).toBeVisible()
+
+    const label = container.querySelector('span')
+
+    expect(label).toBeVisible()
+    expect(label).toHaveStyle('color: #A3ACBC')
+  })
+  it('Render a checkbox checked', () => {
+    const { container } = render(
+      <Checkbox name={'testing-checkbox'} onChange={() => null} checked={true}>
+        Testing
+      </Checkbox>,
+    )
+    const label = container.querySelector('span')
+
+    expect(label).toHaveStyle('color: #2C3644')
   })
 })
