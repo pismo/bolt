98f205d2d85a71f251d167575b6427bcc5d02820
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-04-10 18:35:54 -0300

    feat(pismo-id): add toast on recovery form

--- packages/pismo-id/RecoveryForm.tsx
+++ packages/pismo-id/RecoveryForm.tsx
@@ -1,4 +1,6 @@
 import { colors, styled } from '@pismo/bolt-core'
+import { useToast } from '@pismo/bolt-toast'
+import * as eres from 'eres'
 import { FastField as FormikField, Formik } from 'formik'
 import * as React from 'react'
 import { FormCard } from './components/FormCard'
@@ -6,7 +8,6 @@ import { Input } from './components/Input'
 import { Link } from './components/Link'
 import { SubmitButton } from './components/SubmitButton'
 import { Title } from './components/Title'
-import { Wrapper } from './components/Wrapper'
 import { Yup } from './fns/Yup'
 
 const LinkWrapper = styled.div`
@@ -26,9 +27,7 @@ const InputWrapper = styled.div`
 `
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string()
-    .email()
-    .required(),
+  email: Yup.string().required(),
 })
 
 interface RecoveryFormProps {
@@ -38,52 +37,52 @@ interface RecoveryFormProps {
 }
 
 export const RecoveryForm = (props: RecoveryFormProps) => {
+  const { toast } = useToast()
   const { goToLogin, goToRecoverySuccess, auth } = props
   return (
-    <Wrapper>
-      <FormCard>
-        <Title>{`Esqueceu a senha? Informe seu e-mail para continuar`}</Title>
-        <Formik
-          initialValues={{
-            email: '',
-          }}
-          validationSchema={validationSchema}
-          enableReinitialize
-          onSubmit={async (values, { setSubmitting }) => {
-            setSubmitting(true)
-            const [error] = await auth.recoverPassword(values.email)
+    <FormCard>
+      <Title>{`Esqueceu a senha? Informe seu e-mail para continuar`}</Title>
+      <Formik
+        initialValues={{
+          email: '',
+        }}
+        validationSchema={validationSchema}
+        enableReinitialize
+        onSubmit={async (values, { setSubmitting }) => {
+          setSubmitting(true)
+          const [error] = await eres(auth.recoverPassword(values.email))
 
-            if (error) {
-              setSubmitting(false)
-              return null
-            }
-
+          if (error) {
             setSubmitting(false)
-            return goToRecoverySuccess(values.email)
-          }}
-          render={({ handleChange, isSubmitting, submitForm, isValid }) => (
-            <div>
-              <InputWrapper>
-                <FormikField
-                  name={'email'}
-                  placeholder={'E-mail ou CPF'}
-                  flex={'0.49'}
-                  component={Input}
-                  disabled={isSubmitting}
-                  onChange={handleChange}
-                />
-              </InputWrapper>
-              <SubmitButton onClick={() => submitForm()} disabled={!isValid}>
-                ENVIAR
-              </SubmitButton>
-              <LinkWrapper>
-                <ArrowBack>&larr;</ArrowBack>
-                <Link onClick={() => goToLogin()}>Lembrou?</Link>
-              </LinkWrapper>
-            </div>
-          )}
-        />
-      </FormCard>
-    </Wrapper>
+            toast.error('Erro: usuário não encontrado')
+            return null
+          }
+
+          setSubmitting(false)
+          return goToRecoverySuccess(values.email)
+        }}
+        render={({ handleChange, isSubmitting, submitForm, isValid }) => (
+          <div>
+            <InputWrapper>
+              <FormikField
+                name={'email'}
+                placeholder={'E-mail ou CPF'}
+                flex={'0.49'}
+                component={Input}
+                disabled={isSubmitting}
+                onChange={handleChange}
+              />
+            </InputWrapper>
+            <SubmitButton onClick={() => submitForm()} disabled={!isValid}>
+              ENVIAR
+            </SubmitButton>
+            <LinkWrapper>
+              <ArrowBack>&larr;</ArrowBack>
+              <Link onClick={() => goToLogin()}>Lembrou?</Link>
+            </LinkWrapper>
+          </div>
+        )}
+      />
+    </FormCard>
   )
 }
