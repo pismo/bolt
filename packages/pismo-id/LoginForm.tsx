928c9f2bc191f1a7d3aa832aaf8028bbb8267611
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-04-10 18:34:25 -0300

    feat(pismo-id): add get preferences on login form

--- packages/pismo-id/LoginForm.tsx
+++ packages/pismo-id/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { Checkbox } from '@pismo/bolt-checkbox'
 import { styled } from '@pismo/bolt-core'
+import { useToast } from '@pismo/bolt-toast'
 import * as eres from 'eres'
 import { FastField as FormikField, Formik } from 'formik'
 import * as React from 'react'
@@ -8,7 +9,6 @@ import { Input } from './components/Input'
 import { Link } from './components/Link'
 import { SubmitButton } from './components/SubmitButton'
 import { Title } from './components/Title'
-import { Wrapper } from './components/Wrapper'
 import { Yup } from './fns/Yup'
 
 const LinkWrapper = styled.div`
@@ -29,84 +29,98 @@ const validationSchema = Yup.object().shape({
 
 interface LoginFormProps {
   auth: any
+  tokenRefresher: any
   goToRecovery(): void
   setIsValid(valid: boolean): void
   setTokenRefresher(refresher: any): void
 }
 
 export const LoginForm = (props: LoginFormProps) => {
-  const { goToRecovery, setIsValid, setTokenRefresher, auth } = props
+  const [rememberMeState, setRememberMe] = React.useState<boolean>(false)
+  const [identificationState, setIdentification] = React.useState<string>('')
+
+  const { toast } = useToast()
+
+  const { goToRecovery, setIsValid, setTokenRefresher, auth, tokenRefresher } = props
+
+  React.useEffect(() => {
+    auth.getPreferences().then(response => {
+      const { identification = '', rememberMe = false } = response
+      setIdentification(identification)
+      setRememberMe(rememberMe)
+    })
+  }, [])
 
   return (
-    <Wrapper>
-      <FormCard>
-        <Title>Olá informe seus dados para continuar</Title>
-        <Formik
-          initialValues={{
-            emailOrCPF: '',
-            password: '',
-            rememberMe: false,
-          }}
-          validationSchema={validationSchema}
-          enableReinitialize
-          onSubmit={async (values, { setSubmitting }) => {
-            setSubmitting(true)
-            const [error] = await eres(auth.login(values))
+    <FormCard>
+      <Title>Olá informe seus dados para continuar</Title>
+      <Formik
+        initialValues={{
+          emailOrCPF: identificationState,
+          password: '',
+          rememberMe: rememberMeState,
+        }}
+        validationSchema={validationSchema}
+        enableReinitialize
+        onSubmit={async (values, { setSubmitting }) => {
+          setSubmitting(true)
+          const [error] = await eres(auth.login(values))
 
-            if (error) {
-              setSubmitting(false)
-              return auth.resetSession()
-            }
-
-            const [errorUpdate, result] = await eres(auth.onUpdateProxy())
-
-            if (errorUpdate) {
-              setSubmitting(false)
-              return auth.resetSession()
-            }
-
+          if (error) {
             setSubmitting(false)
-            setTokenRefresher(result)
-            return setIsValid(true)
-          }}
-          render={({ values, setFieldValue, isSubmitting, submitForm, isValid }) => (
-            <div>
-              <InputWrapper>
-                <FormikField
-                  name={'emailOrCPF'}
-                  placeholder={'E-mail'}
-                  flex={'0.49'}
-                  component={Input}
-                  disabled={isSubmitting}
-                />
-                <FormikField
-                  name={'password'}
-                  placeholder={'Senha'}
-                  flex={'0.49'}
-                  component={Input}
-                  disabled={isSubmitting}
-                  type={'password'}
-                />
-              </InputWrapper>
-              <SubmitButton onClick={submitForm} disabled={!isValid}>
-                ENTRAR
-              </SubmitButton>
-              <LinkWrapper>
-                <Checkbox
-                  name={'rememberMe'}
-                  onChange={evt => {
-                    return setFieldValue('rememberMe', evt.target.checked)
-                  }}
-                  checked={values.rememberMe}
-                >
-                  Lembrar
-                </Checkbox>
-                <Link onClick={() => goToRecovery()}>Esqueceu a senha?</Link>
-              </LinkWrapper>
-            </div>
-          )}
-        />
-      </FormCard>
-    </Wrapper>
+            toast.error('Erro: usuário ou senha ínvalidos')
+            return auth.resetSession()
+          }
+
+          const [errorUpdate, result] = await eres(auth.onUpdateProxy(tokenRefresher))
+
+          if (errorUpdate) {
+            setSubmitting(false)
+            toast.error('Erro: algo inesperado ocorreu, por favor tente novamente')
+            return auth.resetSession()
+          }
+
+          setSubmitting(false)
+          setTokenRefresher(result)
+          return setIsValid(true)
+        }}
+        render={({ values, setFieldValue, isSubmitting, submitForm, isValid }) => (
+          <>
+            <InputWrapper>
+              <FormikField
+                name={'emailOrCPF'}
+                placeholder={'E-mail'}
+                flex={'0.49'}
+                component={Input}
+                disabled={isSubmitting}
+              />
+              <FormikField
+                name={'password'}
+                placeholder={'Senha'}
+                flex={'0.49'}
+                component={Input}
+                disabled={isSubmitting}
+                type={'password'}
+              />
+            </InputWrapper>
+            <SubmitButton onClick={submitForm} disabled={!isValid}>
+              ENTRAR
+            </SubmitButton>
+            <LinkWrapper>
+              <Checkbox
+                name={'rememberMe'}
+                onChange={evt => {
+                  return setFieldValue('rememberMe', evt.target.checked)
+                }}
+                checked={values.rememberMe}
+              >
+                Lembrar
+              </Checkbox>
+              <Link onClick={() => goToRecovery()}>Esqueceu a senha?</Link>
+            </LinkWrapper>
+          </>
+        )}
+      />
+    </FormCard>
   )
 }
