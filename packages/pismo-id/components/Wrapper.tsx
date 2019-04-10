d8042626e342a0c295b492c573d8529825fd24d2
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-04-10 18:31:40 -0300

    feat(pismo-id): remove component wrapper

--- packages/pismo-id/components/Wrapper.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from '@pismo/bolt-core'
-
-export const Wrapper = styled.div`
-  display: table-cell;
-  transition: all 0.15s ease-in-out;
-  vertical-align: middle;
-  z-index: 2;
-`
