b6ff78651924f2bacf46bacc4d50d3553150586e
Vinicius <varaujovieira@gmail.com>
2019-04-11 16:01:14 -0300

    Lerna coverage setup test.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+  
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{ts,tsx,js,jsx}',
+    '!**/node_modules/**',
+    '!**/*.d.ts',
+    '!**/index.ts'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
