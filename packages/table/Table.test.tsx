24d6989b7226e3f9178b9e97c693113f94cbecf6
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-04-12 17:00:07 -0300

    fix(table): add test

---
+++ packages/table/Table.test.tsx
@@ -0,0 +1,12 @@
+import 'jest-dom/extend-expect'
+import * as React from 'react'
+import { render } from 'react-testing-library'
+import { Table } from './'
+
+describe('Table test', () => {
+  it('Render a Table', () => {
+    const { container } = render(<Table>Testing</Table>)
+
+    expect(container).toBeVisible()
+  })
+})
