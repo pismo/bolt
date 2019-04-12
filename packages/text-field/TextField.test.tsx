5d29c4021613780dedc71c4d8470c7b0410ee724
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-04-12 15:44:49 -0300

    fix(text-field): tests

--- packages/text-field/TextField.test.tsx
+++ packages/text-field/TextField.test.tsx
@@ -5,7 +5,9 @@ import { TextField } from './TextField'
 
 describe('TextField test', () => {
   it('Render a TextField with label and flex', () => {
-    const { container, getByText } = render(<TextField label="text-field" flex="2.5" />)
+    const { container, getByText } = render(
+      <TextField placeholder="Placeholder" value="" onChange={value => value} label="text-field" flex="2.5" />,
+    )
 
     const textWrapper = container.querySelector('div')
     const input = container.querySelector('input')
@@ -18,7 +20,14 @@ describe('TextField test', () => {
 
   it('Render a TextField with props to input', () => {
     const { container, getByText } = render(
-      <TextField label="text-field" flex="2.5" value="input-test" name="name-test" onChange={e => e.target.value} />,
+      <TextField
+        placeholder="Placeholder"
+        label="text-field"
+        flex="2.5"
+        value="input-test"
+        name="name-test"
+        onChange={e => e.target.value}
+      />,
     )
 
     const textWrapper = container.querySelector('div')
