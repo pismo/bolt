2cfe547929f0305cf40b0a78d97c4edc4b8832d5
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-04-15 20:07:05 -0300

    chore(pismo-id): add back to login link on recovery success

--- packages/pismo-id/RecoverySuccess.tsx
+++ packages/pismo-id/RecoverySuccess.tsx
@@ -1,16 +1,26 @@
+import { colors, styled } from '@pismo/bolt-core'
 import * as React from 'react'
 import { FormCard } from './components/FormCard'
+import { Link } from './components/Link'
 import { Title } from './components/Title'
+
+const ArrowBack = styled.span`
+  font-size: 1.25rem;
+  color: ${colors.grey800};
+`
 
 interface RecoverySuccessProps {
   email: string
+  goToLogin(): void
 }
 
 export const RecoverySuccess = (props: RecoverySuccessProps) => {
-  const { email } = props
+  const { email, goToLogin } = props
   return (
     <FormCard>
       <Title>{`As instruções para redefinição de senha foram enviadas para ${email}`}</Title>
+      <ArrowBack>&larr;</ArrowBack>
+      <Link onClick={() => goToLogin()}>Voltar ao login</Link>
     </FormCard>
   )
 }
