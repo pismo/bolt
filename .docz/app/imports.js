efd5930bf144322ca4bc5bf24c1c88daac031528
Abraao Bueno <abraao.as3@gmail.com>
2019-07-02 17:56:05 -0300

    fixed untracked files

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'packages/core/core.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "packages-core-core" */ 'packages/core/core.mdx'
-    ),
-}
