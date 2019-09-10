1312492dd2e5282b999e9c6307bc96365142bba5
Abraão Bueno <abraao.as3@gmail.com>
2019-09-10 17:32:47 -0300

    remove sdk

--- sdk/auth/auth.test.ts
+++
@@ -1,7 +0,0 @@
-import { Auth } from './auth'
-
-describe('Export auth', () => {
-  it('should export the auth client correctly', () => {
-    expect(Auth).toBeDefined()
-  })
-})
--- sdk/cross-storage/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { CrossStorage } from './index'
-
-describe('Export cross-storage', () => {
-  it('should export the cross-storage client correctly', () => {
-    expect(CrossStorage).toBeDefined()
-  })
-})
