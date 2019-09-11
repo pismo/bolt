3f3e28eb91e4ab602696dd6dc9020a49ecef1eac
Abraão Bueno <abraao.as3@gmail.com>
2019-09-11 15:53:25 -0300

    working in calendar

--- packages/calendar/Calendar.tsx
+++ packages/calendar/Calendar.tsx
@@ -1,21 +1,31 @@
 import * as React from 'react'
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import Calendar from ''
 
 const { useState } = React
 
 interface LocaleMap {
-  [key: string]: any
+  [key: string]: Locale
 }
 
 interface CalendarProps {
-  localeMap: LocaleMap[]
+  localeMap: LocaleMap
   language: keyof LocaleMap
+  label?: string
+  format?: string
+  withIcon?: boolean
+  iconPosition?: 'end' | 'start'
 }
 
 const Calendar: React.FC<CalendarProps> = ({
   localeMap,
-  language
+  language,
+  label,
+  format,
+  withIcon,
+  iconPosition = 'start'
 }: CalendarProps) => {
   const [selectedDate, handleDateChange] = useState(new Date())
   const change = date => {
@@ -23,7 +33,20 @@ const Calendar: React.FC<CalendarProps> = ({
   }
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils} locale={localeMap[language]}>
-      <DatePicker variant='inline' value={selectedDate} onChange={change} />
+      <DatePicker
+        variant='inline'
+        value={selectedDate}
+        onChange={change}
+        label={label}
+        format={format}
+        InputProps={
+          withIcon
+            ? iconPosition === 'start'
+              ? { startAdornment: <InputAdornment position='start' /> }
+              : {}
+            : {}
+        }
+      />
     </MuiPickersUtilsProvider>
   )
 }
