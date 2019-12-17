78b827178d8f39a5ede52bfee9e1b6dc0ab022b4
abraaobuenotype <abraao.as3@gmail.com>
2019-12-17 19:34:22 -0300

    fix unused declarations

--- packages/core/icons/index.tsx
+++ packages/core/icons/index.tsx
@@ -1,3 +1,5 @@
 import * as React from 'react'
 
 import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon'
+
+export const ExampleIcon = (props: SvgIconProps) => <SvgIcon {...props}></SvgIcon>
