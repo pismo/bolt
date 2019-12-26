0fbf38f37eb13e1c5b4a626da0c811c53ff3389b
abraaobuenotype <abraao.as3@gmail.com>
2019-12-26 15:31:03 -0300

    formating

--- packages/core/icons/index.tsx
+++ packages/core/icons/index.tsx
@@ -2,4 +2,4 @@ import * as React from 'react'
 
 import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon'
 
-export const ExampleIcon = (props: SvgIconProps) => <SvgIcon {...props}></SvgIcon>
+export const ExampleIcon = (props: SvgIconProps) => <SvgIcon {...props} />
