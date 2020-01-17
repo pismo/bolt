490862007b091c74274fbdd5a7cf8d01b54fdab8
Abraão Bueno <abraao.as3@gmail.com>
2020-01-17 14:49:36 -0300

    feat: address methods

---
+++ packages/world-format/__test__/WorldFormat.test.ts
@@ -0,0 +1,10 @@
+import { cleanup, wait } from '@testing-library/react'
+import 'jest-dom/extend-expect'
+
+import { WorldFormat, Countrys, Format } from '../WorldFormat'
+
+afterEach(cleanup)
+
+describe('@pismo/bolt-world-format', () => {
+  test('test', async () => {})
+})
