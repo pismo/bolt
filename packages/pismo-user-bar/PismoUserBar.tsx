32fabf448f573f9713139fc76acd629de9b1aa20
Abraão Bueno <abraao.as3@gmail.com>
2020-02-13 12:12:45 -0300

    fix: (PismoUserBar) remove shadow

--- packages/pismo-user-bar/PismoUserBar.tsx
+++ packages/pismo-user-bar/PismoUserBar.tsx
@@ -26,7 +26,8 @@ const useStyles = makeStyles((theme: Theme) => {
       height: '100vh',
       overflow: 'hidden',
       backgroundColor: extra ? extra['background'].main : 'transparent',
-      color: extra ? extra.textField.defaultColor : '#fff'
+      color: extra ? extra.textField.defaultColor : '#fff',
+      boxShadow: 'unset'
     }
   }
 })
