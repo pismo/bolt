84691a83201d0268daf0b9f96226dfc4282d6688
abraaobuenotype <abraao.as3@gmail.com>
2020-03-11 23:40:19 -0300

    chore: adding TextField

--- packages/calendar/Calendar.tsx
+++ packages/calendar/Calendar.tsx
@@ -1,7 +1,23 @@
 import * as React from 'react'
 
-const Calendar: React.FC = () => {
-  return <div>Calendario</div>
+import { DatePicker } from '@material-ui/pickers'
+
+import { TextField } from '@pismo/bolt-text-field'
+
+const { Fragment, useState } = React
+
+const Calendar: React.FC = ({ TextFieldProps }) => {
+  return (
+    <Fragment>
+      <TextField label='selecione a data' value={TextField.value} />
+      <DatePicker
+        label='Basic example'
+        value={selectedDate}
+        onChange={date => handleDateChange(date)}
+        TextFieldComponent={() => <div />}
+      />
+    </Fragment>
+  )
 }
 
 export { Calendar }
