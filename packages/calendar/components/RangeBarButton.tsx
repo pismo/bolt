8ff287482b6be4edadd5aecacc5bb42b8173b61e
abraaobuenotype <abraao.as3@gmail.com>
2021-03-01 20:14:42 -0300

    fix: typescript errors

--- packages/calendar/components/RangeBarButton.tsx
+++ packages/calendar/components/RangeBarButton.tsx
@@ -6,50 +6,52 @@ import Typography from '@material-ui/core/Typography'
 
 import { BoltTheme } from '@pismo/bolt-core'
 
-const useStyles = makeStyles((theme: BoltTheme) => {
-  return {
-    button: ({ selected }: any) => {
-      const _default = {
-        width: '100%',
-        padding: '8px 0px',
-        flexDirection: 'column',
-        position: 'relative',
-        borderRadius: '4px 4px 0px 0px',
-        '&::after': {
-          all: 'unset'
-        }
-      }
-
-      let sel = {}
-
-      if (selected) {
-        sel = {
-          border: `2px solid ${theme.palette.colors.background['50']}`,
-          borderBottom: 'none',
-
+const useStyles = makeStyles(
+  (theme: BoltTheme): any => {
+    return {
+      button: ({ selected }: any) => {
+        const _default = {
+          width: '100%',
+          padding: '8px 0px',
+          flexDirection: 'column',
+          position: 'relative',
+          borderRadius: '4px 4px 0px 0px',
           '&::after': {
-            content: '" "',
-            display: 'block',
-            width: '100%',
-            height: '4px',
-            position: 'absolute',
-            backgroundColor: theme.palette.background.paper,
-            bottom: '-3px'
+            all: 'unset'
           }
         }
-      }
 
-      return { ..._default, ...sel }
-    },
-    label: {
-      color: theme.palette.colors.background['50'],
-      marginBottom: '10px'
-    },
-    date: {
-      fontWeight: 'bold'
+        let sel = {}
+
+        if (selected) {
+          sel = {
+            border: `2px solid ${theme.palette.colors.background['50']}`,
+            borderBottom: 'none',
+
+            '&::after': {
+              content: '" "',
+              display: 'block',
+              width: '100%',
+              height: '4px',
+              position: 'absolute',
+              backgroundColor: theme.palette.background.paper,
+              bottom: '-3px'
+            }
+          }
+        }
+
+        return { ..._default, ...sel }
+      },
+      label: {
+        color: theme.palette.colors.background['50'],
+        marginBottom: '10px'
+      },
+      date: {
+        fontWeight: 'bold'
+      }
     }
   }
-})
+)
 
 export interface RangeBarButtonProps {
   label: string
@@ -64,7 +66,7 @@ const RangeBarButton: React.FC<RangeBarButtonProps> = ({
   onClick,
   selected = false
 }: RangeBarButtonProps) => {
-  const classes = useStyles({ selected })
+  const classes: any = useStyles({ selected })
   return (
     <ButtonBase className={classes.button} onClick={onClick}>
       <Typography className={classes.label} variant='caption'>
