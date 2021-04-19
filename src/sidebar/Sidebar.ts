70a70c4756206b0985176c4059047a927556b6d7
Henrique Otavio <rique34@hotmail.com>
2021-04-19 17:00:57 -0300

    fix: standardizing Code

--- src/sidebar/Sidebar.ts
+++ src/sidebar/Sidebar.ts
@@ -62,7 +62,7 @@ class Sidebar implements ISidebar {
     footerLabel: string,
     enableMoviment: boolean
   ): void => {
-    this.#_container.classList.add(`${enableMoviment ? 'tw-grow-sidebar' : ''}`, 'tw-sidebar');
+    this.#_container.classList.add(`${enableMoviment ? 'tw-sidebar-grow' : ''}`, 'tw-sidebar');
 
     this.#_header.classList.add('tw-sidebar-btn', 'tw-sidebar-btn-l0');
 
@@ -74,11 +74,22 @@ class Sidebar implements ISidebar {
     this.#_container.appendChild(this.#_content);
     this.#_container.appendChild(this.#_footer);
 
+    if (enableMoviment) {
+      const expandContentIcon = document.createElement('div');
+      expandContentIcon.classList.add('tw-sidebar-expand-btn');
+      const expandIcon = document.createElement('span');
+      expandIcon.classList.add('tw-i-chevron-right');
+      expandContentIcon.appendChild(expandIcon);
+
+      this.#_container.appendChild(expandContentIcon);
+      expandContentIcon.addEventListener('click', this.#_clickExpandMenu);
+    }
+
     const hIcon = document.createElement('span');
     const hLabel = document.createElement('p');
     hIcon.classList.add(`tw-i-${header.icon}`, 'tw-sidebar-btn-icon');
 
-    hLabel.classList.add(`${enableMoviment ? 'tw-hide' : ''}`, 'tw-sidebar-btn-label');
+    hLabel.classList.add(`${enableMoviment ? 'tw-sidebar-hide' : ''}`, 'tw-sidebar-btn-label');
     hLabel.innerText = header.label;
 
     this.#_header.appendChild(hIcon);
@@ -86,24 +97,15 @@ class Sidebar implements ISidebar {
 
     const levels = ['tw-sidebar-btn-l0', 'tw-sidebar-btn-l1', 'tw-sidebar-btn-l2'];
 
-    if (enableMoviment) {
-      const expandContentIcon = document.createElement('div');
-      expandContentIcon.classList.add('tw-btn-expand');
-      const expandIcon = document.createElement('span');
-      expandIcon.classList.add('tw-i-chevron-right');
-      expandContentIcon.appendChild(expandIcon);
-
-      this.#_content.appendChild(expandContentIcon);
-      expandContentIcon.addEventListener('click', this.#_clickExpandMenu);
-    }
-
     content.forEach((el, i) => {
       const id = `${i}-${el.level}-${el.label}`;
       const child: HTMLElement & { name: string } = document.createElement('button');
       child.id = id;
       child.name = el.name || '';
 
-      child.classList.add(`${el.level === 2 && enableMoviment && 'tw-hide'}`, 'tw-sidebar-btn', levels[el.level]);
+      const active = !!(el.level === 2 && enableMoviment);
+
+      child.classList.add(`${active && 'tw-sidebar-hide'}`, 'tw-sidebar-btn', levels[el.level]);
       this.#_contentList[id] = child;
       let icon: HTMLElement;
       if (el.icon) {
@@ -113,7 +115,7 @@ class Sidebar implements ISidebar {
       }
 
       const label = document.createElement('p');
-      label.classList.add(`${enableMoviment ? 'tw-hide' : ''}`, 'tw-sidebar-btn-label');
+      label.classList.add(`${enableMoviment ? 'tw-sidebar-hide' : ''}`, 'tw-sidebar-btn-label');
       label.innerText = el.label;
 
       child.appendChild(label);
@@ -138,24 +140,24 @@ class Sidebar implements ISidebar {
     if (this.#_onSelected) this.#_onSelected(target);
   };
 
-  #_findItems = (item): void => {
-    item.forEach((res: HTMLElement) => res.classList.toggle('tw-hide'));
+  #_findItems = (item: NodeListOf<Element>): void => {
+    item.forEach((res) => res.classList.toggle('tw-sidebar-hide'));
   };
 
   #_removeClass = (): void => {
-    this.#_content.querySelector('.tw-btn-expand span')?.classList.remove('tw-i-chevron-right');
-    this.#_content.querySelector('.tw-btn-expand span')?.classList.remove('tw-i-chevron-left');
+    this.#_content.querySelector('.tw-sidebar-expand-btn span')?.classList.remove('tw-i-chevron-right');
+    this.#_content.querySelector('.tw-sidebar-expand-btn span')?.classList.remove('tw-i-chevron-left');
   };
 
   #_clickExpandMenu = (): void => {
-    const active = this.#_container.classList.contains('tw-grow-sidebar');
+    const active = this.#_container.classList.contains('tw-sidebar-grow');
     this.#_removeClass();
 
     document
-      .querySelector('.tw-btn-expand span')
+      .querySelector('.tw-sidebar-expand-btn span')
       ?.classList.add(`${active ? 'tw-i-chevron-left' : 'tw-i-chevron-right'}`);
 
-    this.#_container.classList.toggle('tw-grow-sidebar');
+    this.#_container.classList.toggle('tw-sidebar-grow');
     this.#_findItems(this.#_container.querySelectorAll('.tw-sidebar-btn-label'));
     this.#_findItems(this.#_container.querySelectorAll('.tw-sidebar-btn-l2'));
   };
