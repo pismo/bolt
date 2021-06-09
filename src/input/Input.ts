ee4a3dc794dccf73a39c23bf9c7803e3c7355483
dmarinss <diogo.marins@pismo.io>
2021-06-09 17:27:25 -0300

    feat: developing, refining, documentation

--- src/input/Input.ts
+++ src/input/Input.ts
@@ -1,6 +1,7 @@
 export interface IInputDataset {
   name?: string;
   label?: string;
+  tooltip?: string;
   type?: string;
   helpertext?: string;
   starticon?: string;
@@ -45,9 +46,9 @@ class Input implements IInput {
 
   #label: HTMLElement;
 
-  #help: HTMLElement;
+  #containerTooltip: HTMLElement;
 
-  #helpText: HTMLElement;
+  #tooltip: HTMLElement;
 
   #helperText: HTMLElement;
 
@@ -79,20 +80,19 @@ class Input implements IInput {
     this.#container = ref;
     this.#container.classList.add('tw-input-container');
 
-    const containerLabel = document.createElement('div');
-    containerLabel.classList.add('tw-flex');
-
     this.#label = document.createElement('label');
     this.#label.classList.add('tw-input-label');
 
-    this.#help = document.createElement('div');
-    this.#help.classList.add('tw-i-dialog-help');
-    this.#help.classList.add('tw-help');
+    this.#containerTooltip = document.createElement('div');
+    this.#containerTooltip.classList.add('tw-i-dialog-help'); // Alterar ícone
+    this.#containerTooltip.classList.add('tw-tooltip');
 
-    this.#helpText = document.createElement('span');
+    const tooltipArrow = document.createElement('div');
+    tooltipArrow.classList.add('tw-tooltip-arrow');
+    this.#containerTooltip.appendChild(tooltipArrow);
 
-    const arrow = document.createElement('div');
-    arrow.classList.add('tw-help-arrow');
+    this.#tooltip = document.createElement('span');
+    this.#containerTooltip.appendChild(this.#tooltip);
 
     this.#containerInput = document.createElement('div');
     this.#containerInput.classList.add('tw-input-containerInput');
@@ -120,15 +120,6 @@ class Input implements IInput {
       this.#updateOptions(key, options);
     });
 
-    this.#help.appendChild(this.#helpText);
-    this.#help.appendChild(arrow);
-
-    containerLabel.appendChild(this.#label);
-    containerLabel.appendChild(this.#help);
-
-    this.#container.appendChild(containerLabel);
-    // this.#container.appendChild(this.#label);
-    // this.#container.appendChild(this.#help);
     this.#containerInput.appendChild(this.#input);
     this.#container.appendChild(this.#containerInput);
     this.#container.appendChild(this.#helperText);
@@ -153,9 +144,35 @@ class Input implements IInput {
     switch (key) {
       case 'label':
         this.#label.innerText = options.label ?? '';
-        this.#helpText.innerText =
-          'Mussum Ipsum, cacilds vidis litro abertis. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Suco de cevadiss deixa as pessoas mais interessantis. Manduma pindureta quium dia nois paga. A ordem dos tratores não altera o pão duris.';
+        this.#container.appendChild(this.#label);
         break;
+
+      case 'tooltip': {
+        if (this.#label.innerHTML === '') break;
+
+        const container = document.createElement('div');
+        container.classList.add('tw-flex');
+        container.classList.add('tw-items-center');
+        container.classList.add('tw-relative');
+        container.appendChild(this.#label);
+
+        const fakeLabel = this.#label.cloneNode(true) as HTMLElement;
+        fakeLabel.style.visibility = 'hidden';
+        fakeLabel.style.position = 'absolute';
+        document.body.appendChild(fakeLabel);
+
+        const fakeLabelWidth = fakeLabel.offsetWidth;
+
+        this.#containerTooltip.style.left = `${fakeLabelWidth + 10}px`;
+        container.appendChild(this.#containerTooltip);
+
+        this.#tooltip.innerText = options.tooltip ?? '';
+
+        fakeLabel.remove();
+        this.#container.appendChild(container);
+        break;
+      }
+
       case 'helpertext':
         this.#helperText.innerText = options.helpertext ?? '';
         break;
